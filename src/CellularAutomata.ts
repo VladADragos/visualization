b8fc2951b919b52a414c1c2e972b6280069685dc
VladADragos <vlad6620@yahoo.com>
2020-07-29 19:40:58 +0200

    backup

--- src/CellularAutomata.ts
+++ src/CellularAutomata.ts
@@ -1,5 +1,21 @@
-class CellularAutomata {
+class CellularAutomata
+{
+  cellSize: number = 40;
+  height = 4;
+  width = 4;
 
+  constructor(width: number, height: number)
+  {
+    this.height = height;
+    this.width = width;
+  }
+
+  getArray2d(): IDrawable[]
+  {
+    throw new Error('Method not implemented.');
+  }
+
+  nextGen(): void { }
 }
 
 export default CellularAutomata;
